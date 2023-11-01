afc2b4d393be135ebaa498b4994d28adf8b69ee1
Blasnjacobo <blasnjacobo05@gmail.com>
2023-10-31 19:35:15 -0700

    finished but needs some changes

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,21 +1,30 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { categories } from './Data'
 
 const CategoryList = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-4 mt-10'>
-      <Link
-        to={`/category/${algo}`}
-        className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2x1 overflow-hidden bg-gradient-to-r from-purple-500 to-pink-500 transition -all hover:scale-105'
-      >
-        <div className='p-5 flex justify-center items-center'>
-          <img src={algo} alt='Categoria' className='x-36' />'
-        </div>
-        <h1 className='text-2x1 font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
-          {algo}
-        </h1>
-      </Link>
+      {categories.map(categories =>
+        <Link
+          to={`/quiz-app/category/${categories.category}`}
+          className={`flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2x1 overflow-hidden bg-gradient-to-r ${categories.color} transition -all hover:scale-105`}
+        >
+          <div className='p-5 flex justify-center items-center'>
+            <img
+              src={categories.img}
+              alt={`Category ${categories.science}`}
+              className='w-36'
+            />'
+          </div>
+          <h1 className='text-2x1 font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5'>
+            {categories.category}
+          </h1>
+        </Link>
+      )}
     </div>
   )
 }
--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,9 +1,62 @@
-import React from 'react'
+/* eslint-disable eqeqeq */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { questions, imgs } from '../components/Data'
+import Question from '../components/Question'
+import quiz from '../assets/quiz.jpg'
 
+const shuffleArray = (array) => {
+  const RandomQuestions = array.sort(() => Math.random() - 0.5)
+  return RandomQuestions.slice(0, 5)
+}
 export const CategoryPage = () => {
+  const { category } = useParams()
+
+  const [questionsFiltered, setQuestionsFiltered] = useState(
+    // eslint-disable-next-line eqeqeq
+    questions.filter(question => question.category == category))
+
+  const [indexQuestion, setIndexQuestion] = useState(0)
+  const [activeQuiz, setActiveQuiz] = useState(false)
+
+  useEffect(() => {
+    const newQuestions = shuffleArray(questionsFiltered)
+    setQuestionsFiltered(newQuestions)
+    console.log(questions)
+    console.log(questionsFiltered)
+  }, [])
   return (
-    <div>
-      CategoryPage
+    <div
+      className='container flex flex-col items-center justify-center gap-10'
+      style={{ height: 'calc(100vh - 5rem)' }}
+    >
+      {activeQuiz
+        ? (
+          <Question
+            filteredQuestion={questionsFiltered[indexQuestion]}
+            setIndexQuestion={setIndexQuestion}
+            indexQuestion={indexQuestion}
+            questionsFiltered={questionsFiltered}
+            setActiveQuiz={setActiveQuiz}
+          />)
+        : (
+          <>
+            <h1 className='text-3xl text-teal-900 text-center font-bold'>
+              {category}
+            </h1>
+            <div className='flex justify-center items-center'>
+              <img src={quiz} alt={category} className='w-72' />
+            </div>
+            <button
+              className='text-white bg-gray-900 py-2 rounded-lg font-bold px-5 transition-all hover:bg-yellow-500 hover:text-gray-900'
+              onClick={() => setActiveQuiz(true)}
+            >
+              Iniciar quiz
+            </button>
+          </>
+          )}
     </div>
   )
 }
